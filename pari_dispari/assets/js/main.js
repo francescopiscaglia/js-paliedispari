333152af03845775cac17025cc595c7a4259de89
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-04 15:25:01 +0200

    add base structur

--- pari_dispari/assets/js/main.js
+++ pari_dispari/assets/js/main.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+// 1. Preparazione 
+// creo una variabile per salvare l'input dell'utente
+
+// creo una variabile per il numero inserito dell'utente
+
+
+// 2. Analisi dei dati
+// dichiaro le due variabili
+
+// stampo le variabili in console
+
+
+// 3. Elaborazione codice
+// creo una funzione per generare un numero random da 1 a 5 per il pc
+
+// creo una funzione per sommare i due numeri
+
+// verifico se sono pari o dispari
+
+// 4. Output

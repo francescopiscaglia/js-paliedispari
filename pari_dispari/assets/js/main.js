e0e95edded025c212ee15ca93627d79289d25897
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-04 15:30:22 +0200

    add variable

--- pari_dispari/assets/js/main.js
+++ pari_dispari/assets/js/main.js
@@ -1,14 +1,17 @@
 // 1. Preparazione 
 // creo una variabile per salvare l'input dell'utente
+let user_choose;
 
 // creo una variabile per il numero inserito dell'utente
-
+let user_number;
 
 // 2. Analisi dei dati
 // dichiaro le due variabili
+user_choose = prompt("Ciao, scegli tra pari o dispari");
+user_number = Number(prompt("Ora inserisci un numero da 1 a 5"));
 
 // stampo le variabili in console
-
+console.log(user_choose, user_number);
 
 // 3. Elaborazione codice
 // creo una funzione per generare un numero random da 1 a 5 per il pc

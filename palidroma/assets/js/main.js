ae6a6f0030e2a5b431ef1f956dcdb0815db5bccc
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-04 14:08:52 +0200

    add user_word variable and prompt

--- palidroma/assets/js/main.js
+++ palidroma/assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+// 1. Preparazione 
+// creo la variabile per salvare la parola scritta dall'utente
+let user_word;
+
+
+// 2. Analisi dei dati
+// dichiaro la variabile con un prompt
+user_word = prompt("Ciao, inserisci una parola");
+
+// stampo la parola in console
+console.log(user_word);
+
+
+// 3. Elaborazione del codice
+// creo una funzione per verificare se è palindroma o no
+
+
+// 4. Output

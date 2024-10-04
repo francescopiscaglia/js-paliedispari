9a01402f99f6aeb40eb751045e456015e4fe548d
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-04 15:55:10 +0200

    delete variable unused

--- pari_dispari/assets/js/main.js
+++ pari_dispari/assets/js/main.js
@@ -5,11 +5,6 @@ let user_choose;
 // creo una variabile per il numero inserito dell'utente
 let user_number;
 
-// 2. Analisi dei dati
-// dichiaro le due variabili
-// user_choose = prompt("Ciao, scegli tra pari o dispari");
-// user_number = Number(prompt("Ora inserisci un numero da 1 a 5"));
-
 // stampo le variabili in console
 console.log(user_choose, user_number);
 
@@ -20,16 +15,17 @@ for (let i = 1; i <= 3; i++) {
     user_number = Number(prompt("Ora inserisci un numero da 1 a 5 compresi"));
     console.log(user_choose, user_number);
 
+    // se la scelta dell'utente è corretta e il numero inserito è compreso tra 1 e 5
     if ((user_choose === "pari" || user_choose === "dispari") && (user_number >= 1 && user_number <= 5)) {
-        correct_choose = true;
+        // se è vero puoi proseguire
         alert("Puoi proseguire");
 
+        // e termina il ciclo
         break
     } else {
+        // se no riprova fino un massimo di tre tentativi
         alert("Attenzione, la scelta deve essere 'pari' o 'dispari' e il numero compreso tra 1 e 5");
     }
-    
-    
 }
 // creo una funzione per generare un numero random da 1 a 5 per il pc
 
